910da34b6428f62b60a5c5ac3e1c5dffcf7d747f
hiki934 <129893072+hiki934@users.noreply.github.com>
2023-12-31 10:08:04 -0500

    java

---
+++ java/texto.java
@@ -0,0 +1,17 @@
+package java;
+import java.util.Scanner;
+
+public class texto {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+          System.out.print("Ingrese un número: ");
+          int numero = sc.nextInt();
+
+          if (numero % 2 == 0) {
+              System.out.println("El número es divisible entre 2");
+          } else {
+              System.out.println("El número no es divisible entre 2");
+          }
+        }
+    }
+}
