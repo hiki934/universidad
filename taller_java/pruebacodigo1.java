22dcff8a7d155a473f23f9066dd978b22ce19ac6
hiki934 <fsosabazan@gmail.com>
2024-02-28 10:01:49 -0500

    java

--- taller_java/pruebacodigo1.java
+++ taller_java/pruebacodigo1.java
@@ -2,7 +2,7 @@ package taller_java;
 
 public class pruebacodigo1{
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("hola mundooo");
     }
     
 }
